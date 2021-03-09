91ad4b46bc30fc32fa6812b39f5ec49fffbd31b5
hirotaka <hirotakamadomagi@icloud.com>
2021-03-10 00:34:07 +0900

    create model to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'pages#index'
   get 'pages/help'
   get 'pages/link'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
