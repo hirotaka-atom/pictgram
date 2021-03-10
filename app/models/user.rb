5533339503aaa9c253306dd07d61e37999c43c0f
hirotaka <hirotakamadomagi@icloud.com>
2021-03-10 11:03:26 +0900

    add validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
+  VALID_PASSWORD_REGEX = /\A(?=\d{0,99}+[a-z])(?=[a-z]{0,99}+\d)[a-z\d]{8,100}+\z/i
+  validates :password, presence: true, length: {minimum: 8, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
 end
